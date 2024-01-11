2937a35f8e923302aaf6abafd66f24f904a9e2a3
mizo0122 <148730198+mizo0122@users.noreply.github.com>
2024-01-11 10:33:37 +0900

    更新

---
+++ src/php/db_connect.php
@@ -0,0 +1,8 @@
+<?php
+const SERVER = 'mysql220.phy.lolipop.lan';
+const DBNAME = 'LAA1517346-final';
+const USER = 'LAA1517346';
+const PASS = 'Pass0122';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
