be7de7bb530ee64b8f041d0334c04a186be2a2a7
mizo0122 <148730198+mizo0122@users.noreply.github.com>
2024-01-15 10:53:13 +0900

    変更

---
+++ src/php/all.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>曲一覧</title>
+</head>
+<body>
+    <?php
+    $pdo = new PDO('mysql:host=mysql220.phy.lolipop.lan;dbname=LAA1517346-final;charset=utf8','LAA1517346','PASS0122')
+    $pdo->query('select*from MUSIC')
+    foreach($pdo->query('select*from MUSIC') as $row){
+        echo '';
+        echo $row['music_id'],':';
+        echo $row['music_name'],':';
+        echo $row['artist_id'];
+        echo '</p>';
+    }
+    ?>
+</body>
+</html>
